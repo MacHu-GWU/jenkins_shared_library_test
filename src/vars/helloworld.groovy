a1b0c031d714a60877ebd1ae52dbe8cc97567f64
SanheHu <4038571439@GSA.GOV>
2020-11-13 19:18:21 -0500

    Create helloworld.groovy

---
+++ src/vars/helloworld.groovy
@@ -0,0 +1,14 @@
+def call(Closure body) {
+    node {
+        stage("Build") {
+            sh "echo install"
+        }
+        stage("Test") {
+            sh "echo test"
+        }
+        stage("Deploy") {
+            sh "echo publish"
+        }
+        body()
+    }
+}
