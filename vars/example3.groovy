a66f8b402bd9d8115558c97ac032da3c5b3ea00f
SanheHu <4038571439@GSA.GOV>
2020-11-16 20:48:02 -0500

    debug

--- vars/example3.groovy
+++ vars/example3.groovy
@@ -1,22 +1,15 @@
 def call(Closure postHelloScript) {
-    pipeline {
-        agent {
-            node {
-                label ""
-            }
-        }
-        stages {
-            stage("Hello World") {
-                steps {
-                    script {
-                        sh """
-                            echo Hello
-                        """
-                    }
+    node {
+        stage("Hello World") {
+            steps {
+                script {
+                    sh """
+                        echo Hello
+                    """
                 }
             }
-            postHelloScript()
         }
+        postHelloScript()
     }
 }
 
